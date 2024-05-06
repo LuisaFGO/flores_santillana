0c913e7b86ea4046749481f1465c6ef0e82adc71
LuisaFGO <fgiraldo070@gmail.com>
2024-05-05 21:32:19 -0500

    add logo in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
   isOpen = true;
 
+  logofs = 'assets/logofs.png';
+
   navDeploy() {
     this.isOpen = !this.isOpen;
   }
-
 }
