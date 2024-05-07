7966835bf2220b8334502ef429f2300955b49719
LuisaFGO <fgiraldo070@gmail.com>
2024-05-06 22:36:00 -0500

    update home & styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,17 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  
-
   icons = [
-    {icon:'assets/icons/highQuality.png', text: 'High quality'},
-    {icon:'assets/icons/price.png', text: 'Best price'},
-    {icon:'assets/icons/distribution.png', text: 'Direct from our crops'}
-  ]
+    { icon: 'assets/icons/highQuality.png', text: 'High quality' },
+    { icon: 'assets/icons/bestPrice.png', text: 'Best price' },
+    { icon: 'assets/icons/distribution.png', text: 'Direct from our crops' },
+  ];
 
   flor = 'assets/fotos/floresMuestra.jpg';
-
 }
