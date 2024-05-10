e82d97bad20af0c019e67eee3b3191b202cab97c
LuisaFGO <fgiraldo070@gmail.com>
2024-05-09 22:44:04 -0500

    add message wsp

---
+++ src/app/components/message-wsp/message-wsp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-wsp',
+  templateUrl: './message-wsp.component.html',
+  styleUrls: ['./message-wsp.component.css']
+})
+export class MessageWspComponent {
+
+}
