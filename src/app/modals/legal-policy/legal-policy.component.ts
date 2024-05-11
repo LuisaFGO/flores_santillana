c7eb9c2e918fb09e0c85945f9c5d667338af5b1d
LuisaFGO <fgiraldo070@gmail.com>
2024-05-10 23:25:38 -0500

    add models & shared

---
+++ src/app/modals/legal-policy/legal-policy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legal-policy',
+  templateUrl: './legal-policy.component.html',
+  styleUrls: ['./legal-policy.component.css']
+})
+export class LegalPolicyComponent {
+
+}
---
+++ src/app/modals/policy-cookies/policy-cookies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-policy-cookies',
+  templateUrl: './policy-cookies.component.html',
+  styleUrls: ['./policy-cookies.component.css']
+})
+export class PolicyCookiesComponent {
+
+}
