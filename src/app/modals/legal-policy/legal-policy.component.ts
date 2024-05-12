2ea811dd58a6ce18732d18043ee78b7154445331
LuisaFGO <fgiraldo070@gmail.com>
2024-05-11 22:31:12 -0500

    modal legalPolicy

--- src/app/modals/legal-policy/legal-policy.component.ts
+++ src/app/modals/legal-policy/legal-policy.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-legal-policy',
   templateUrl: './legal-policy.component.html',
-  styleUrls: ['./legal-policy.component.css']
+  styleUrls: ['./legal-policy.component.css'],
 })
-export class LegalPolicyComponent {
-
-}
+export class LegalPolicyComponent {}
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { LegalPolicyComponent } from 'src/app/modals/legal-policy/legal-policy.component';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  constructor(private modalService: NgbModal) {}
+
+  openLegalModal() {
+    this.modalService.open(LegalPolicyComponent);
+  }
+
   logo = 'assets/logos/logoBlanco.png';
 }
