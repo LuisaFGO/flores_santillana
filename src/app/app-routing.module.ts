136c5db74e67d1a5aaa231321e234e1e5d1635a8
LuisaFGO <fgiraldo070@gmail.com>
2024-05-12 22:14:35 -0500

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'aboutUs', component: AboutUsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'gallery', component: GalleryComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
