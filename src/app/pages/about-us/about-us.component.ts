438ec4f9830c8ab40dfce0a81eec6450e7704b26
LuisaFGO <fgiraldo070@gmail.com>
2024-05-13 00:21:11 -0500

    add html aboutUs

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  styleUrls: ['./about-us.component.css'],
 })
 export class AboutUsComponent {
-
+  PhotoCar = 'assets/aboutUs/1.jpg';
+  woman = 'assets/aboutUs/2.jpg';
 }
