64ee00579606446a1c54bfbbf989c53ee1694854
LuisaFGO <fgiraldo070@gmail.com>
2024-05-22 22:05:06 -0500

    add reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -31,7 +32,7 @@ import { DetailProductComponent } from './modals/detail-product/detail-product.c
     ContactComponent,
     DetailProductComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  formContact: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.formContact = this.formBuilder.group({
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      terms: [false, Validators.requiredTrue],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  submitForm() {
+    if (this.formContact.valid) {
+      console.log(this.formContact.value);
+    }
+  }
 }
