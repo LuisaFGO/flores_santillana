f4e8f07aa147adfdb63d8f77060d359033016a22
LuisaFGO <fgiraldo070@gmail.com>
2024-05-26 21:15:18 -0500

    add logic carousel in detail

--- src/app/modals/detail-product/detail-product.component.ts
+++ src/app/modals/detail-product/detail-product.component.ts
@@ -9,9 +9,25 @@ export class DetailProductComponent {
   @Input() product: any;
   @Output() closeModal = new EventEmitter<void>();
   showModal = false;
+  currentSlide = 0;
 
   closeModalEvent() {
     this.showModal = false;
     this.closeModal.emit();
+  }
+  prevSlide() {
+    if (this.currentSlide === 0) {
+      this.currentSlide = this.product?.photos.length - 1;
+    } else {
+      this.currentSlide--;
+    }
+  }
+
+  nextSlide() {
+    if (this.currentSlide === this.product?.photos.length - 1) {
+      this.currentSlide = 0;
+    } else {
+      this.currentSlide++;
+    }
   }
 }
