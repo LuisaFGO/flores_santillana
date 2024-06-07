c0c62d944eb921697f085e1087cbba98f7f43d01
LuisaFGO <fgiraldo070@gmail.com>
2024-06-06 22:48:17 -0500

    add & updated images

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -2,20 +2,20 @@ import { Component } from '@angular/core';
 import { NgbCarousel, NgbSlide } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-    selector: 'app-carousel',
-    templateUrl: './carousel.component.html',
-    styleUrls: ['./carousel.component.css'],
-    standalone: true,
-    imports: [NgbCarousel, NgbSlide]
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css'],
+  standalone: true,
+  imports: [NgbCarousel, NgbSlide],
 })
 export class CarouselComponent {
-
   imagesArray = [
-    'assets/fotos/floresParaiso.jpg',
+    'assets/fotos/bParadise.jpg',
     'assets/fotos/shoquinAzul.jpg',
     'assets/fotos/shoquinWhite.jpg',
     'assets/fotos/ruscusIsraeli.jpg',
-  ]
+    'assets/fotos/floresParaiso.jpg',
+  ];
 
-  images = this.imagesArray.map(img => img);
+  images = this.imagesArray.map((img) => img);
 }
