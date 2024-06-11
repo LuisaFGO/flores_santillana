b0096d234c7de4b52acdeec97f0ea8af3ce48812
LuisaFGO <fgiraldo070@gmail.com>
2024-06-10 22:05:14 -0500

    adding content to the blog

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.css'],
 })
 export class BlogComponent {
+  showMoreContent = false;
 
+  img = 'assets/imgProducts/birdP.jpg';
+
+  toggleMoreContent() {
+    this.showMoreContent = !this.showMoreContent;
+  }
 }
