8f1dc6cbf7dd2559faa349f0fdc18c9c8674bf72
LuisaFGO <fgiraldo070@gmail.com>
2024-06-12 21:04:15 -0500

    add setting for railway

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("./dist/nombre-de-tu-app"));
+app.use(express.static("./dist/flores_santillana"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/nombre-de-tu-app/" })
+  res.sendFile("index.html", { root: "dist/flores_santillana" })
 );
 
 app.listen(process.env.PORT || 8080);
