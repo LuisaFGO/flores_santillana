8d853d9875a31cf0d0ef8fc049769d2384af0d27
LuisaFGO <fgiraldo070@gmail.com>
2024-06-15 21:06:30 -0500

    updated env.d

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,17 +7,7 @@ interface ImportMeta {
 }
 
 interface ImportMetaEnv {
-  readonly NG_APP_ENV: string;
-
   [key: string]: any;
   readonly NG_APP_LOCAL_HOST: string;
   readonly NG_APP_URL: string;
 }
-
-// declare var process: {
-//   env: {
-//     NG_APP_ENV: string;
-//     [key: string]: any;
-//     readonly GENERAL: string;
-//   };
-// };
