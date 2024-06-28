414079911ed90aa5f5bcf4bb44b484434dc637b7
LuisaFGO <fgiraldo070@gmail.com>
2024-06-27 22:48:07 -0500

    test

--- server.js
+++ server.js
@@ -15,4 +15,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/flores_santillana/index.html"));
 });
 
+app.use((req, res, next) => {
+  console.log(`Received request: ${req.method} ${req.url}`);
+  next();
+});
+
 app.listen(process.env.PORT || 8080);
